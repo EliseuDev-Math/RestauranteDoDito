5973a6d749696a9bd8e664db147a1bfbc1feace8
EliseuDev-Math <eliseualexandre496@gmail.com>
2026-09-22 11:29:31 -0400

    Add files via upload

---
+++ RestauranteDito/restaurante/src/main/java/br/com/dito/restaurante/DTO/ProdutoDTO.java
@@ -0,0 +1,29 @@
+package br.com.dito.restaurante.DTO;
+
+public class ProdutoDTO {
+
+    private Long id;
+    private String nome;
+    private Double preco;
+
+
+    public ProdutoDTO() {
+    }
+
+
+    public ProdutoDTO(Long id, String nome, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Double getPreco() { return preco; }
+    public void setPreco(Double preco) { this.preco = preco; }
+}
